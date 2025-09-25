f832da58d792b322bd250de7261e809b1b80e0d2
vee <964746347@qq.com>
2025-09-25 10:42:31 +0800

    视频图形采集界面新增、编辑、查看详情页面字段遮挡 任务2973

--- src/views/video/modules/TabAiClickpicSettingForm.vue
+++ src/views/video/modules/TabAiClickpicSettingForm.vue
@@ -85,11 +85,11 @@
          },
         labelCol: {
           xs: { span: 24 },
-          sm: { span: 5 },
+          sm: { span: 8 },
         },
         wrapperCol: {
           xs: { span: 24 },
-          sm: { span: 16 },
+          sm: { span: 13 },
         },
         confirmLoading: false,
         validatorRules: {
